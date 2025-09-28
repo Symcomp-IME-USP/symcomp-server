78c62979e9ff7bc1971e41a691c84ff5a9819647
theinstantago <douradotiago44@gmail.com>
2025-09-27 21:07:53 -0300

    fix(entrypoint.sh): dependency error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
 
-echo "Waiting for database to be ready..."
-python wait-for-db.py
-
 echo "Making sure dependencies are up to date..."
 pip install --upgrade pip
 pip install --no-cache-dir -r requirements.txt
+
+echo "Waiting for database to be ready..."
+python wait-for-db.py
 
 echo "Automatically generating migrations..."
 python manage.py makemigrations --noinput
